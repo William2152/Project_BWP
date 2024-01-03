ee65a83ecde37acf3de5b1c1f0e72abb598a7532
Ryu Alv <ryualvino57@gmail.com>
2024-01-03 18:11:51 +0700

    betulin gambar buat toko

--- projectBWP_front_end_10p/laravel/resources/views/Toko/gakpunyatoko.blade.php
+++ projectBWP_front_end_10p/laravel/resources/views/Toko/gakpunyatoko.blade.php
@@ -3,8 +3,8 @@
     <div class="col-10 rounded" style="background-color: whitesmoke; height: 30vw; margin-bottom: 2vw;">
         <div class="container" style="margin-top: 2vw; margin-bottom: 2vw; ">
             <div class="store ">
-                <img src="/store.png" style="width: 100px; height: 100px; margin-top: 10vw; margin-left: 45%;" alt=""
-                    srcset="">
+                <img src="{{ asset('assets/category/store.png') }}"
+                    style="width: 100px; height: 100px; margin-top: 10vw; margin-left: 45%;" alt="" srcset="">
                 <p class="text-center ms-2">Anda Belum Memiliki Toko Silahkan Membuat Toko Terlebih Dahulu !</p>
 
                 <a href="{{ url('/profile/formtoko') }}" class="btn btn-primary text-light" name="btnCreate"
