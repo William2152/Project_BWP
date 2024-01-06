be0e3ac5ec1ed31f0c3d2ed2f9930e48de453e7e
William2152 <133088365+William2152@users.noreply.github.com>
2024-01-06 22:41:43 +0700

    update tampilan voucher

--- projectBWP_front_end_10p/laravel/resources/views/User/voucherSaya.blade.php
+++ projectBWP_front_end_10p/laravel/resources/views/User/voucherSaya.blade.php
@@ -1,31 +1,29 @@
 @extends('template.profilePage')
 
 @section('konten')
-<div class="col-10" style="background-color: whitesmoke; height: auto; margin-bottom: 2vw;">
-    <div class="container" style="display: flex; flex-direction: column; justify-content: center;">
-        <h2 style="margin-top: 0.8vw; margin-left: 0.3vw; margin-bottom: 1vw;">Voucher Saya</h2>
-        @for($i = 0; $i < 10; $i++)
-            <div class="voucherBoxContainer" style="display: flex; flex-direction: row; margin-bottom: 1vw;">
-                <div class="voucherBox" style="display: flex; flex-direction: row; width: 27vw; height: 5vw; background-color: red; padding-left: 0.5vw; padding-top: 0.5vw;">
-                    <div class="voucherImage" style="width: 7vw; height: 4vw; background-color: #fff; text-align: center; padding-top: 1.5vw;">
-                        <h6>Image</h6>
-                    </div>
-                    <div class="voucherDesc" style="width: 18.5vw; height: 4vw; background-color: #fff; text-align: left; padding-top: 0.5vw; margin-left: 0.5vw; padding-left: 0.5vw;">
-                        <h6>Judul Voucher :</h6>
-                        <h6>Expired Date :</h6>
+    <div class="col-10" style="background-color: whitesmoke; height: auto; margin-bottom: 2vw;">
+        <div class="container" style="display: flex; flex-direction: column; justify-content: center;">
+            <h2 style="margin-top: 0.8vw; margin-left: 0.3vw; margin-bottom: 1vw;">Voucher Saya</h2>
+            @for ($i = 0; $i < 10; $i++)
+                <div class="container mt-5" style="background-color: cyan; height: 22vh; margin-bottom: 2vh;">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="/assets/voucher/70_persen.png" style="width: 20vh; height: 20vh; margin-top: 1vh;"
+                                class="img-fluid" alt="Voucher Image">
+                        </div>
+
+                        <div class="col-md-4" style="padding-top: 1vh;">
+                            <h3>Nama Voucher</h3>
+                            <p><strong>Expired Date:</strong> January 31, 2024</p>
+                            <button class="btn btn-primary">Gunakan Voucher</button>
+                        </div>
+                        <div class="col-md-6" style="padding-top: 10vh;">
+                            <p><strong>Syarat & Ketentuan Berlaku</strong></p>
+                            <p>Pembayaran Wajib Menggunakan Saldo</p>
+                        </div>
                     </div>
                 </div>
-                <div class="voucherBox" style="display: flex; flex-direction: row; width: 27vw; height: 5vw; background-color: red; padding-left: 0.5vw; padding-top: 0.5vw; margin-left: 2.5vw;">
-                    <div class="voucherImage" style="width: 7vw; height: 4vw; background-color: #fff; text-align: center; padding-top: 1.5vw;">
-                        <h6>Image</h6>
-                    </div>
-                    <div class="voucherDesc" style="width: 18.5vw; height: 4vw; background-color: #fff; text-align: left; padding-top: 0.5vw; margin-left: 0.5vw; padding-left: 0.5vw;">
-                        <h6>Judul Voucher :</h6>
-                        <h6>Expired Date :</h6>
-                    </div>
-                </div>
-            </div>
-        @endfor
+            @endfor
+        </div>
     </div>
-</div>
 @endsection
